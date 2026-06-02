f517721fdeb52c73fc8d64d1b90705b69a8cff9d
rioryfox <shesdow@icloud.com>
2026-06-03 02:12:21 +0300

    Load from HF01MS-16 on 2026-06-03 02:12:21

--- my_cast_v0.21.sh
+++ my_cast_v0.21.sh
@@ -8,10 +8,12 @@
 set -e
 clear
 
+#YADISK_TOKEN = ""
 open_file=true
 rebooter=false
 poweroffer=false
 SCRIPT_DIR="$(pwd)"
+
 
 #---Functions
 
@@ -104,25 +106,25 @@ backup_to_sd() {
 
 upload_to_yadisk() {
     local FILE="$1"
-    local TOKEN="$YADISK_TOKEN" 
-    
+    local TOKEN="$YADISK_TOKEN"
+
     if [ -z "$TOKEN" ]; then
         echo " YADISK_TOKEN не задан, пропускаем"
         return 1
     fi
-    
+
     local FILENAME=$(basename "$FILE")
     echo "Загружаем $FILENAME на Яндекс.Диск..."
-    
+
     local UPLOAD_URL=$(curl -s -H "Authorization: OAuth $TOKEN" \
         "https://cloud-api.yandex.net/v1/disk/resources/upload/?path=app:/fbi_backups/$FILENAME&overwrite=true" \
         | jq -r '.href')
-    
+
     if [ -z "$UPLOAD_URL" ] || [ "$UPLOAD_URL" = "null" ]; then
         echo "Не удалось получить ссылку для загрузки"
         return 1
     fi
-    
+
     curl -s -T "$FILE" "$UPLOAD_URL"
     echo "Загружено: https://disk.yandex.ru/app/fbi_backups/$FILENAME"
 }
